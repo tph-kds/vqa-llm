2a88c0fbfabefc951a801ed2f342fe7dee08a9a3
tph-kds <hungcompo123@gmail.com>
2024-07-03 16:03:41 +0700

    feat: resolve error line 9 on package.json file

---
+++ commitlint.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+      'type-enum': [
+        2,
+        'always',
+        [
+          'build',
+          'chore',
+          'ci',
+          'docs',
+          'feat',
+          'fix',
+          'perf',
+          'refactor',
+          'revert',
+          'style',
+          'test'
+        ]
+      ],
+      'subject-case': [0, 'never', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']]
+    }
+  };
