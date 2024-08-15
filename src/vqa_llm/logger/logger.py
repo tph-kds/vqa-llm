18aaf90a405409c8107e06a6dc070d2cc059955c
tph-kds <hungcompo123@gmail.com>
2024-08-16 00:12:17 +0700

    fix: adjust readme file

---
+++ src/vqa_llm/logger/logger.py
@@ -0,0 +1,81 @@
+import os
+import sys
+import logging
+
+class LoggerHandler(logging.Handler):
+    def __init__(self):
+        super().__init__()
+        self.log = ""
+
+    def reset(self):
+        self.log = ""
+
+    def emit(self, record):
+        if record.name == "httpx":
+            return
+        
+        log_entry = self.fotmat(record)
+        self.log += log_entry
+        self.log += "\n\n"
+
+
+class MainLoggerHandler(LoggerHandler):
+
+    def __init__(self, 
+                 name, 
+                 format_logging,
+                 datefmt_logging,
+                 log_dir,
+                 name_file_logs):
+        
+        super().__init__()
+        self.name = name  
+        self.format_logging = format_logging  
+        self.datefmt_logging = datefmt_logging 
+        self.log_dir = log_dir 
+        self.name_file_logs = name_file_logs 
+
+    def reset_logging(self):
+        r"""
+        Removes basic config of root logger
+        """
+        root = logging.getLogger()
+        list(map(root.removeHandler, root.handlers))
+        list(map(root.removeFilter, root.filters))
+
+
+    def get_logger(self) -> logging.Logger:
+        log_filepath = os.path.join(self.log_dir,self.name_file_logs)
+        os.makedirs(self.log_dir, exist_ok=True)
+
+        # logger = logging.getLogger("vqa_logger")
+        formatter = logging.Formatter(
+            fmt=self.format_logging,
+            datefmt=self.datefmt_logging
+        )
+        stream_handler = logging.StreamHandler(sys.stdout)
+        stream_handler.setFormatter(formatter)
+        
+        file_handler = logging.FileHandler(log_filepath)
+        file_handler.setFormatter(formatter)
+        
+        logger = logging.getLogger(self.name)
+        logger.setLevel(logging.INFO)
+        
+        logger.addHandler(stream_handler)
+        logger.addHandler(file_handler)
+
+        return logger
+
+format_logging = "[%(asctime)s : %(levelname)s: %(module)s : %(message)s]"
+datefmt_logging = "%m/%d/%Y %H:%M:%S"
+
+log_dir = "logs"
+name_file_logs = "running_logs.log"
+
+logger = MainLoggerHandler(name = "vqa_logger",
+                           format_logging=format_logging,
+                            datefmt_logging=datefmt_logging,
+                            log_dir=log_dir,
+                            name_file_logs=name_file_logs)
+
